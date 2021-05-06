8839b531fc72fe85170147b7d75f8530da82d625
evandroase <evandroase@gmail.com>
2021-05-05 21:43:45 -0300

    troca imagem alterado

--- script/trocaimagem.js
+++ script/trocaimagem.js
@@ -1,21 +1,37 @@
-const imagem = window.document.createElement("img");
+let imagemElemento = document.querySelector(".imagem");
 const tempo = new Date();
-const res = document.querySelector("#res");
 
-function trocaImagem() {
-  let seg = tempo.getSeconds();
+const imagens = ["../imagens/morumbi.jpg", "../imagens/vilabelmiro.jpeg", "../imagens/arenacorinthians.jpeg", "../imagens/allianzparque.jpeg"]
+let index = 0;
 
-  if (seg < 15) {
-    imagem.setAttribute("src", "imagens/allianzparque.jpeg");
-  } else if (seg < 30) {
-    imagem.setAttribute("src", "imagens/morumbi.jpg");
-  } else if (seg < 45) {
-    imagem.setAttribute("src", "imagens/vilabelmiro.jpeg");
-  } else {
-    imagem.setAttribute("src", "imagens/arenacorinthians.jpeg");
-  }
 
-  res.appendChild(imagem);
+if(imagemElemento == null) {
+  console.log("imagem vazia!")
 }
 
-setInterval(trocaImagem(), 15000);
+function trocar() {
+  imagemElemento.src = imagens[index];
+  index > 1 ? index = 0 : index++;
+}
+
+window.onload = function () {
+  setInterval(trocar, 1000);
+};
+
+// function trocaImagem() {
+//   let seg = tempo.getSeconds();
+
+//   if (seg < 5) {
+//     imagem.setAttribute("src", "../imagens/allianzparque.jpeg");
+//   } else if (seg < 15) {
+//     imagem.setAttribute("src", "../imagens/morumbi.jpg");
+//   } else if (seg < 20) {
+//     imagem.setAttribute("src", "../imagens/vilabelmiro.jpeg");
+//   } else {
+//     imagem.setAttribute("src", "../imagens/arenacorinthians.jpeg");
+//   }
+
+// }
+
+
+
